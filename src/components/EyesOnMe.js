cc658ade57e91d9684c8113641438b80f230d87e
J Vance <justinleevance94@gmail.com>
2024-01-09 10:23:38 -0500

    Core Deliverables

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+function EyesOnMe () {
+    function printGood (){
+        console.log("Good!")
+    }
+    function printFocus () {
+        console.log("Hey! Eyes on me!")
+    }
+    return (
+        <button onFocus={printGood} onBlur={printFocus}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function printText() {
+        console.log("Entering password...")
+    }
+
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={printText} />
+        </div>
     )
 }
 
